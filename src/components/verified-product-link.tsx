03a7d5dfeb241ddd49f7ec633d90f6f0eff65102
juaniulian <juanulian@gmail.com>
2025-08-18 23:23:11 +0000

    que el boton comprar redirija directamente al formulario

--- src/components/verified-product-link.tsx
+++ src/components/verified-product-link.tsx
@@ -1,16 +1,5 @@
 'use client'
 
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
 import { Button } from '@/components/ui/button'
 
 interface VerifiedProductLinkProps {
@@ -21,27 +10,13 @@ export default function VerifiedProductLink({
   className = ""
 }: VerifiedProductLinkProps) {
   
+  const handleClick = () => {
+    window.open('https://forms.gle/CVdyFmBcASjXRKww7', '_blank')
+  }
+
   return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button className={className}>
-          Comprar
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Simulación de Compra Finalizada</AlertDialogTitle>
-          <AlertDialogDescription>
-            La simulación de compra ha finalizado. ¿Avanzamos a la encuesta de satisfacción?
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cerrar</AlertDialogCancel>
-          <AlertDialogAction onClick={() => window.open('https://forms.gle/CVdyFmBcASjXRKww7', '_blank')}>
-            Avanzar
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <Button onClick={handleClick} className={className}>
+      Comprar
+    </Button>
   )
 }
